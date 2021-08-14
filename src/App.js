969713f63ce7567d069a6ebd02e8a88d41fcfec0
cllcnkrt <cllcnkrt@outlook.com>
2021-08-14 12:57:48 +0300

    navbarElements(styled component) nav, navbarContainer, navlogo done.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import {
+  BrowserRouter as Router,
+  
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router >
+      <Navbar/>
       
-    </div>
+    </Router>
   );
 }
 
